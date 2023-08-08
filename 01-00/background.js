6e9edb853f15354848bfea738ff0a36897a22350
Robert Kao <kyp001@yahoo.com.tw>
2023-08-08 14:00:12 +0800

    01-00 basic fucntion

--- 01-00/background.js
+++ 01-00/background.js
@@ -1,7 +1,14 @@
-chrome.runtime.onInstalled.addListener(() => {
+chrome.runtime.onInstalled.addListener((tab) => {
+	console.log(tab)
 	console.log('installed')
 })
 
 chrome.bookmarks.onCreated.addListener(() => {
 	console.log('Bookmark saved')
 })
+
+// 不需要有此 function
+// chrome.action.onClicked.addListener( () => {
+// 	console.log('chrome action click')
+// })
+
--- 01-00/content.js
+++ 01-00/content.js
@@ -1,3 +1,13 @@
 window.onload = () => {
-	document.querySelector("#buttons").prepend("Dark mode")
+	const button = document.createElement('button');
+	button.id = 'darkModeButton';
+	button.textContent = "DO IT DACK";
+	// add button ,若使用 #buttons 最後不能顯示,可能是原程式蓋掉加入之button
+	// document.querySelector("#buttons").prepend(button);
+	document.querySelector("#end").prepend(button);
+	button.addEventListener('click', () => enableDarkMode());
+}
+
+function enableDarkMode() {
+	document.getElementsByTagName('ytd-app')[0].style.backgroundColor= 'black';
 }
