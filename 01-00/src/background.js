ca4ceafbe82594bbb95714d17d51fbb42ee5199d
Robert Kao <kyp001@yahoo.com.tw>
2023-08-16 16:28:11 +0800

    01-00 for version 3 , background just 1 file

--- 01-00/src/background.js
+++ 01-00/src/background.js
@@ -1,5 +1,3 @@
-import * as message from "./backgroundMessaging.js"
-
 
 chrome.runtime.onInstalled.addListener((tab) => {
 	console.log(tab)
@@ -15,3 +13,21 @@ chrome.bookmarks.onCreated.addListener(() => {
 // 	console.log('chrome action click')
 // })
 
+// chrome.bookmarks.onMoved.addListener(() => {
+// 	console.log('Bookmark moved')
+// })
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+	console.log('message', message);
+	console.log('sender',sender);
+	sendResponse({ content: "background response" });
+});
+
+
+chrome.bookmarks.onMoved.addListener(() => {
+	// send to active windows
+	chrome.tabs.query({ active: true, currentWindow: true}, 
+		tabs => {
+			chrome.tabs.sendMessage(tabs[0].id, {name: 'Robert'});
+		}
+	);
+});
