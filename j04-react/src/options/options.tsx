7affb65deafca2c3fa8d2572b69810e66cb3a4e4
Robert Kao <kyp001@yahoo.com.tw>
2023-08-29 14:03:06 +0800

    1. j04-react add css loader  and icon

--- j04-react/src/options/options.tsx
+++ j04-react/src/options/options.tsx
@@ -1,14 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import css
-import './popup.css'
+import './options.css'
 
 // const test = <p>Hello Wrold!</p>
 const test = <img src="icon.png" />
 
-// 1. from template.html
-// const root = ReactDOM.createRoot(document.getElementById('root'))
-// 2. create div element
+// create div element
 const rootElement = document.createElement('div')
 document.body.appendChild(rootElement)
 const root = ReactDOM.createRoot(rootElement)
--- j04-react/webpack.config.js
+++ j04-react/webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
   devtool: 'cheap-module-source-map',
   entry: {
     popup: path.resolve('src/popup/popup.tsx'),
+    options: path.resolve('src/options/options.tsx'),
   },
   module: {
     rules: [
@@ -14,6 +15,14 @@ module.exports = {
         use: 'ts-loader',
         test: /\.tsx?$/,
         exclude: /node_modules/,
+      },
+      {
+        use: ['style-loader', 'css-loader'],
+        test: /\.css$/i,
+      },
+      {
+        type: 'asset/resource',
+        test: /\.(jpg|jpeg|png|woff|woff2|eof|svg)$/,
       },
     ],
   },
@@ -24,14 +33,19 @@ module.exports = {
           from: path.resolve('src/manifest.json'),
           to: path.resolve('dist'),
         },
+        {
+          from: path.resolve('src/static'),
+          to: path.resolve('dist'),
+        },
       ],
     }),
-    new HtmlPlugin({
-      title: 'React Extension',
-      filename: 'popup.html',
-      template: 'src/popup/template.html',
-      chunks: ['popup'],
-    }),
+    // new HtmlPlugin({
+    //   title: 'React Extension',
+    //   filename: 'popup.html',
+    //   template: 'src/popup/template.html',
+    //   chunks: ['popup'],
+    // }),
+    ...getHTMLPlugins(['popup', 'options']),
   ],
   resolve: {
     extensions: ['.tsx', '.tx', '.js'],
@@ -41,4 +55,21 @@ module.exports = {
     filename: '[name].js',
     path: path.resolve('dist'),
   },
+  // share chynks modulea
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
+}
+
+function getHTMLPlugins(chunks) {
+  return chunks.map(
+    (chunk) =>
+      new HtmlPlugin({
+        title: 'React Extension',
+        filename: `${chunk}.html`,
+        chunks: [chunk],
+      })
+  )
 }
