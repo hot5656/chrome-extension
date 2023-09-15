629c41b204773465445097b45f79a47f5d67ed68
Robert Kao <kyp001@yahoo.com.tw>
2023-09-15 14:33:49 +0800

    j06-adblock-v3 change to declarativeNetRequest mode

--- j06-adblock-extension-v3/src/contentScript/contentScript.tsx
+++ j06-adblock-extension-v3/src/contentScript/contentScript.tsx
@@ -11,32 +11,33 @@
 // 	]
 // },
 
-// 對特定網頁執行特定功能 %?%
-const rules: {
-  [url: string]: () => void
-} = {
-  'https://www.nytimes.com/section/opinion/editorials':
-    filterNYTOpinionEeditorials2,
-}
+// mask js do adblock
+// // 對特定網頁執行特定功能 %?%
+// const rules: {
+//   [url: string]: () => void
+// } = {
+//   'https://www.nytimes.com/section/opinion/editorials':
+//     filterNYTOpinionEeditorials2,
+// }
 
-// block by id
-function filterNYTOpinionEeditorials() {
-  const app = document.getElementById('site-content')
-  const wrapper = document.getElementById('top-wrapper')
-  app.removeChild(wrapper)
-}
+// // block by id
+// function filterNYTOpinionEeditorials() {
+//   const app = document.getElementById('site-content')
+//   const wrapper = document.getElementById('top-wrapper')
+//   app.removeChild(wrapper)
+// }
 
-// block by class
-function filterNYTOpinionEeditorials2() {
-  const divs = document.getElementsByTagName('div')
-  for (const div of divs) {
-    if (div.className.indexOf('ad') != -1) {
-      div.style.display = 'none'
-    }
-  }
-}
+// // block by class
+// function filterNYTOpinionEeditorials2() {
+//   const divs = document.getElementsByTagName('div')
+//   for (const div of divs) {
+//     if (div.className.indexOf('ad') != -1) {
+//       div.style.display = 'none'
+//     }
+//   }
+// }
 
-if (document.URL in rules) {
-  console.log('document.URL:', document.URL)
-  rules[document.URL]()
-}
+// if (document.URL in rules) {
+//   console.log('document.URL:', document.URL)
+//   rules[document.URL]()
+// }
