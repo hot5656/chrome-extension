491a78cbaef46222dae6e768d6748e2c7197d747
Robert Kao <kyp001@yahoo.com.tw>
2023-11-24 16:31:27 +0800

    n06-edx-dual-subtitle update 2023/11/24

--- n06-edx-dual-subtitle/src/contentScript/contentScript.tsx
+++ n06-edx-dual-subtitle/src/contentScript/contentScript.tsx
@@ -19,10 +19,13 @@ window.addEventListener('load', function () {
 
 function checkInterval() {
   intervalRun = true
-  activerCount = 0
+  activerCount = 1
   const intervalId = setInterval(() => {
     let iframeElement = document.querySelector('iframe#unit-iframe')
     if (iframeElement) {
+      // iframeElement.removeAttribute('referrerpolicy')
+      // console.log("iframeElement", iframeElement)
+
       let preButton = document.querySelector(
         '.previous-btn.btn.btn-link'
       ) as HTMLButtonElement
@@ -71,29 +74,58 @@ function checkInterval() {
         lectureSvgElement.style.height = '32px'
         lectureSvgElement.style.color = 'red'
 
-        console.log('wait...')
-        setTimeout(() => {
-          console.log('run...')
-          let firstChild = document.querySelector('.closed-captions')
-          let bodyElement = document.querySelector('.video-wrapper')
-          console.log('firstChild', firstChild)
-          console.log('bodyElement', bodyElement)
-          // // add div
-          // const firstChild = document.querySelector('.closed-captions')
-          // console.log('firstChild', firstChild)
-          // if (firstChild) {
-          //   const bodyElement = document.querySelector('.video-wrapper')
-          //   const rootElement = document.createElement('div')
-          //   console.log('bodyElement', bodyElement)
-          //   console.log('rootElement', rootElement)
-          //   rootElement.id = 'second-subtitle'
-          //   // rootElement.className = 'horizontal-box'
-          //   bodyElement.insertBefore(rootElement, firstChild)
+        {
+          let videoElement = document.querySelector(
+            '.wrapper-downloads .video-sources'
+          ) as HTMLAnchorElement
+          let srtElement = document.querySelector(
+            '.wrapper-download-transcripts .btn-link'
+          )
+          if (videoElement) {
+            console.log('videoElement', videoElement.href)
+          }
+          if (srtElement) {
+            console.log('srtElement', videoElement.href)
+          }
+        }
 
-          //   const root = createRoot(rootElement)
-          //   root.render(<SecondSubtitle />)
-          // }
-        }, 10000)
+        let btnElements = document.querySelectorAll('.btn-link')
+        console.log('btnElements:', btnElements)
+
+        // intervalSubtitleRun = false
+        // if (intervalSubtitleRun) {
+        //   setTimeout(() => {
+        //     checkIntervalSubtitle()
+        //   }, INTERVAL_STEP + 100)
+        // } else {
+        //   checkIntervalSubtitle()
+        // }
+
+        // checkIntervalSubtitle()
+
+        // console.log('wait...')
+        // setTimeout(() => {
+        //   console.log('run...')
+        //   let firstChild = document.querySelector('.closed-captions')
+        //   let bodyElement = document.querySelector('.video-wrapper')
+        //   console.log('firstChild', firstChild)
+        //   console.log('bodyElement', bodyElement)
+        //   // // add div
+        //   // const firstChild = document.querySelector('.closed-captions')
+        //   // console.log('firstChild', firstChild)
+        //   // if (firstChild) {
+        //   //   const bodyElement = document.querySelector('.video-wrapper')
+        //   //   const rootElement = document.createElement('div')
+        //   //   console.log('bodyElement', bodyElement)
+        //   //   console.log('rootElement', rootElement)
+        //   //   rootElement.id = 'second-subtitle'
+        //   //   // rootElement.className = 'horizontal-box'
+        //   //   bodyElement.insertBefore(rootElement, firstChild)
+
+        //   //   const root = createRoot(rootElement)
+        //   //   root.render(<SecondSubtitle />)
+        //   // }
+        // }, 10000)
       } else {
         let lectureIconSvgElements = document.querySelectorAll(
           'a.btn.btn-link>svg'
@@ -127,6 +159,51 @@ function checkInterval() {
     }
     console.log(` ${activerCount * INTERVAL_STEP} ms....`)
     activerCount++
+  }, INTERVAL_STEP)
+}
+
+let intervalSubtitleRun = false
+let activerSubtilleCount = 1
+function checkIntervalSubtitle() {
+  intervalSubtitleRun = true
+  activerSubtilleCount = 1
+  const intervalId = setInterval(() => {
+    console.log(` ${activerSubtilleCount * INTERVAL_STEP} ms....`)
+
+    let lectureElement = document.querySelector('.active.btn')
+    console.log('lectureElement', lectureElement)
+
+    // let iframe = document.querySelector(
+    //   'iframe#unit-iframe'
+    // ) as HTMLIFrameElement
+    // console.log('iframeElement', iframe)
+    // if (iframe) {
+    //   let tcElement = iframe.contentDocument.querySelector('div.tc-wrapper')
+    //   // let firstChild = iframe.contentDocument.querySelector(
+    //   //   'div.closed-captions'
+    //   // )
+    //   // let bodyElement =
+    //   // iframe.contentDocument.querySelector('div.video-wrapper')
+    //   console.log('tcElement4', tcElement)
+    //   // console.log('firstChild4', firstChild)
+    //   // console.log('bodyElemen4', bodyElement)
+    // }
+
+    let iframe = document.querySelector(
+      'iframe#unit-iframe'
+    ) as HTMLIFrameElement
+
+    console.log('iframeElement', iframe)
+
+    if (iframe && iframe.contentDocument) {
+      // Check if the iframe and its contentDocument are available
+      let tcElement = iframe.contentDocument.querySelector('div.tc-wrapper')
+      console.log('tcElement4', tcElement)
+    } else {
+      console.log('iframe or contentDocument is null')
+    }
+
+    activerSubtilleCount++
   }, INTERVAL_STEP)
 }
 
